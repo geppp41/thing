8e7a8685128a163e0f09278dfbf134a0e5af1995
geppp41 <103062160+geppp41@users.noreply.github.com>
2024-02-27 14:09:17 -0800

    Update mtest.js

--- mtest.js
+++ mtest.js
@@ -1,3 +1,10 @@
-div = document.createElement("div")
+div = document.createElement("Button")
+div.id = "div"
 div.innerHTML = "hello There"
 document.body.appendChild(div)
+script = document.createElement("script")
+script.src = `
+d = document.getElementById("div")
+d.addEventListener("click", => {
+  alert("hello")
+});
