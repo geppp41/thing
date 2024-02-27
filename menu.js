be02086f8e4d3df02b2d16311c128509faee0fb9
geppp41 <103062160+geppp41@users.noreply.github.com>
2024-02-27 12:59:04 -0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,13 @@
-
+function menu() {
+  div = document.createElement("div");
+  div.id = "Mmenu";
+  document.body.appendChild(div);
+  mmenu = document.getElementById("Mmenu");
+  button = document.createElement("button");
+  button.id = "button";
+  document.mmenu.appendChild(button);
+  btn = document.getElementById("button");
+  btn.addEventListener("click", => {
+    alert("hello")
+  });
+}
