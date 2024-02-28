620163575ad3ec35b24ce1a2d3eeea68d2143428
geppp41 <103062160+geppp41@users.noreply.github.com>
2024-02-28 09:41:01 -0800

    Update mtest.js

--- mtest.js
+++ mtest.js
@@ -1,13 +1,8 @@
+//javascript:fetch(`https://raw.githubusercontent.com/geppp41/thing/main/mtest.js`).then(data=>{data.text().then(text=>{eval(text)})});
 div = document.createElement("Button")
 div.id = "div"
 div.innerHTML = "hello There"
 document.body.appendChild(div)
 script = document.createElement("script")
-script.src = `
-function menu() {
-  d = document.getElementById("div")
-  d.addEventListener("click", => {
-    alert("hello")
-  });
-}
-`
+script.src = "https://raw.githubusercontent.com/geppp41/thing/main/mtest2.js"
+document.body.appendChild(script)
